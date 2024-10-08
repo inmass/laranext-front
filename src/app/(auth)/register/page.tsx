afb90cbca0dacd195cb95ac4b9e616f9280d7ad1
iinmass <inmass.idbel@gmail.com>
2024-10-08 22:03:34 +0100

    add form validation to register page

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -6,58 +6,83 @@ import InputError from '@/components/InputError';
 import Label from '@/components/Label';
 import Link from 'next/link';
 import { useAuth } from '@/hooks/auth';
-import { useState, FormEvent, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import SectionDivider from '@/components/SectionDivider';
 import SocialLoginButtons from '@/components/SocialLoginButtons';
 import CardLayout from '@/components/layouts/CardLayout';
 import { useTranslations } from 'next-intl';
 import ClipLoader from 'react-spinners/ClipLoader';
 import { PhoneInput } from '@/components/ui/phone-input';
+import { useForm, Controller } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { cn } from '@/lib/utils';
 
-interface Errors {
-  name?: string[];
-  email?: string[];
-  phone?: string[];
-  password?: string[];
-  password_confirmation?: string[];
-}
+
+const registerSchema = (t: (key: string) => string) => z.object({
+  name: z.string().min(1, { message: t('validation.nameRequired') }),
+  email: z.string().email({ message: t('validation.emailInvalid') }),
+  phone: z.string().min(1, { message: t('validation.phoneRequired') })
+  .refine((value) => {
+    // return /^\+212\d*$/.test('+'+value) && value.length == 12;
+    return /^212\d{9}$/.test(value);
+  }, { message: t('validation.phoneInvalid') }),
+  password: z.string().min(8, { message: t('validation.passwordMinLength') }),
+  password_confirmation: z.string().min(1, { message: t('validation.confirmPasswordRequired') })
+}).refine((data) => data.password == data.password_confirmation, {
+    message: t('validation.passwordsMismatch'),
+    path: ["password_confirmation"],
+})
+
+const placeholderT = (key: string) => key;
+const initialSchema = registerSchema(placeholderT);
+type FormData = z.infer<typeof initialSchema>;
 
 const Page = () => {
   const t = useTranslations('Register');
-  const { register } = useAuth({
+  const schema = registerSchema(t);
+  const { register: registerUser } = useAuth({
     middleware: 'guest',
     redirectIfAuthenticated: '/dashboard',
   });
 
-  const [name, setName] = useState<string>('');
-  const [email, setEmail] = useState<string>('');
-  const [phone, setPhone] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [passwordConfirmation, setPasswordConfirmation] = useState<string>('');
-  const [errors, setErrors] = useState<Errors>({});
-  const [loading, setLoading] = useState<boolean>(false);
-  const phoneInputRef = useRef<HTMLInputElement>(null);
-  
-  const submitForm = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const { register, control, handleSubmit, setError, formState: { errors, isValid, isSubmitting } } = useForm<FormData>(
+    {
+      defaultValues: {
+        name: '',
+        email: '',
+        phone: '+212',
+        password: '',
+        password_confirmation: '',
+      },
+      resolver: zodResolver(schema),
+      mode: 'onChange',
+    }
+  );
 
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = (data: FormData) => {
     setLoading(true);
-
-    register({
-      name,
-      email,
-      phone,
-      password,
-      password_confirmation: passwordConfirmation,
-      setErrors,
+    registerUser({
+      ...data,
+      setErrors: (errors) => {
+        Object.keys(errors).forEach((key) => {
+          setError(key as keyof FormData, { 
+            type: 'manual', 
+            message: errors[key][0] 
+          });
+        });
+      },
     });
+    setLoading(false);
   };
 
-  useEffect(() => {
-    if (Object.keys(errors).length > 0) {
-      setLoading(false);
-    }
-  }, [errors]);
+  // useEffect(() => {
+  //   if (Object.keys(errors).length > 0) {
+  //     setLoading(false);
+  //   }
+  // }, [errors]);
 
   return (
     <CardLayout
@@ -65,91 +90,75 @@ const Page = () => {
       title={t('title')}
       description={t('description')}
     >
-      <form onSubmit={submitForm}>
-        {/* Name */}
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <Label htmlFor="name">{t('name')}</Label>
-
           <Input
+            {...register('name')}
+            className={cn(errors.name ? 'border-red-500' : '')}
             id="name"
             type="text"
-            value={name}
-            className="block mt-1 w-full"
-            onChange={(event) => setName(event.target.value)}
-            required
-            autoFocus
           />
-
-          <InputError messages={errors.name} className="mt-2" />
+          <InputError messages={errors.name?.message ? [errors.name.message] : []} className="mt-2" />
         </div>
 
         {/* Email Address */}
         <div className="mt-4">
           <Label htmlFor="email">{t('email')}</Label>
-
           <Input
+            {...register('email')}
             id="email"
             type="email"
-            value={email}
-            className="block mt-1 w-full"
-            onChange={(event) => setEmail(event.target.value)}
-            required
+            className={cn(errors.email ? 'border-red-500' : '')}
           />
-
-          <InputError messages={errors.email} className="mt-2" />
+          <InputError messages={errors.email?.message ? [errors.email.message] : []} className="mt-2" />
         </div>
 
         {/* Phone */}
         <div className="mt-4">
           <Label htmlFor="phone">{t('phone')}</Label>
-
-
-          <PhoneInput
-            value={phone || ''}
-            ref={phoneInputRef}
-            onChange={(value) => setPhone(value)}
-            inputProps={{
-              required: true,
-            }}
+          <Controller
+            name="phone"
+            control={control}
+            rules={{ required: 'Phone is required' }}
+            render={({ field }) => (
+              <PhoneInput
+                value={field.value}
+                onChange={field.onChange}
+                countryCodeEditable={false}
+                inputProps={{
+                  required: true,
+                  ref: field.ref
+                }}
+              />
+            )}
           />
-
-          <InputError messages={errors.phone} className="mt-2" />
+          <InputError messages={errors.phone?.message ? [errors.phone.message] : []} className="mt-2" />
         </div>
 
         {/* Password */}
         <div className="mt-4">
           <Label htmlFor="password">{t('password')}</Label>
-
           <Input
             id="password"
             type="password"
-            value={password}
-            className="block mt-1 w-full"
-            onChange={(event) => setPassword(event.target.value)}
-            required
+            className={cn(errors.password ? 'border-red-500' : '')}
+            {...register('password')}
             autoComplete="new-password"
           />
-
-          <InputError messages={errors.password} className="mt-2" />
+          <InputError messages={errors.password?.message ? [errors.password.message] : []} className="mt-2" />
         </div>
 
         {/* Confirm Password */}
         <div className="mt-4">
-          <Label htmlFor="passwordConfirmation">{t('confirmPassword')}</Label>
-
+          <Label htmlFor="password_confirmation">{t('confirmPassword')}</Label>
           <Input
-            id="passwordConfirmation"
+            {...register('password_confirmation')}
+            id="confirmPassword"
             type="password"
-            value={passwordConfirmation}
-            className="block mt-1 w-full"
-            onChange={(event) => setPasswordConfirmation(event.target.value)}
-            required
+            className={cn(errors.password_confirmation ? 'border-red-500' : '')}
           />
-
-          <InputError
-            messages={errors.password_confirmation}
-            className="mt-2"
-          />
+          <InputError messages={errors.password_confirmation?.message ? [errors.password_confirmation.message] : []} className="mt-2" />
         </div>
 
         <div className="flex items-center justify-end mt-4">
@@ -160,8 +169,8 @@ const Page = () => {
             {t('alreadyRegistered')}
           </Link>
 
-          <Button className="ml-4" disabled={loading}>
-            {loading ? <ClipLoader className='text-white' size={20} /> : t('registerButton')}
+          <Button className="ml-4" disabled={isSubmitting || !isValid || loading}>
+            {isSubmitting ? <ClipLoader className='text-white' size={20} /> : t('registerButton')}
           </Button>
         </div>
       </form>
